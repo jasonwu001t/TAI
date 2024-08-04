d1aad143efe4d4342258bed9c7b4be630551f815
jasonwu001t <jasonwu001t@gmail.com>
2024-08-04 08:32:58 -0700

    updated atom

--- TAI/data/atom.py
+++ TAI/data/atom.py
@@ -1,19 +1,26 @@
+"""
+Focus on 
+1.. user should only call for market data API from atom
+2. market data handlering only from various of brokers and market data providers
+3. Standardize Output data structure, input can be vary due to broker outputs
+"""
+
 # atom.py
 import pandas as pd
-from TAI.broker.alpaca import Alpaca as BrokerAlpaca
+from TAI.broker.alpaca import Alpaca
 from TAI.broker.ib import IB as BrokerIB
 
 class Atom:
     def __init__(self, source='alpaca'):
         if source == 'alpaca':
-            self.handler = BrokerAlpaca()
+            self.handler = Alpaca()
         elif source == 'ib':
             self.handler = BrokerIB()
         else:
             raise ValueError("Unsupported source. Choose either 'alpaca' or 'ib'.")
 
     def get_market_data(self, symbol, start_date, end_date, timeframe='1D'):
-        if isinstance(self.handler, BrokerAlpaca):
+        if isinstance(self.handler, Alpaca):
             return self.handler.get_barset([symbol], timeframe, (pd.to_datetime(end_date) - pd.to_datetime(start_date)).days)
         elif isinstance(self.handler, BrokerIB):
             return self.handler.get_historical_daily_bar(symbol, start_date, end_date)
