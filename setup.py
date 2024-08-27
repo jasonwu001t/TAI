ab225ce8cde864b7803b660a64507d2831be5fb0
jasonwu001t <jasonwu001t@gmail.com>
2024-08-27 15:08:50 -0700

    created requirements.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+# Read the content of requirements.txt
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="TAI",
     version="0.2.0",
@@ -10,34 +14,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/yourusername/TAI",
     packages=find_packages(include=["TAI", "TAI.*"]),
-    install_requires=[
-        "pandas",
-        "numpy",
-        "scipy",
-        "statsmodels",
-        "plotly",
-        "requests",
-        "psycopg2-binary",
-        "mysql-connector-python",
-        "boto3",
-        "openai",
-        "ib_insync",
-        "alpaca-py",
-        "robin_stocks",
-        "fredapi",
-        "flask",
-        'streamlit',
-        'fastapi',
-        'langchain',
-        'langchain-core',
-        'langchain_aws',
-        'langchain_community',
-        'slack_bolt',
-        'polars',
-        'torch',
-        'torchvision',
-        'sentence_transformers'
-    ],
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -46,10 +23,8 @@ setup(
     package_data={
         '': ['*.ini'],
     },
-    python_requires=">=3.11",
+    python_requires=">=3.10",
 )
-
-
 
 """
 After code update, update setup.py version, and install_required
