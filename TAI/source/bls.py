a9c9a8f7bdd70bc8b8f4d8d7f536b61dbd2caf3e
jasonwu001t <jasonwu001t@gmail.com>
2024-09-06 18:13:37 -0700

    update

--- TAI/source/bls.py
+++ TAI/source/bls.py
@@ -1,6 +1,8 @@
 """IF Error, check if added api_key"""
+
 from TAI.utils import ConfigLoader
-import requests, json
+import requests
+import json
 import pandas as pd
 from datetime import datetime
 
@@ -17,19 +19,21 @@ class BLS:
         auth = BLSAuth()
         self.api_key = auth.get_api_key()
         self.lookback_years = 5
+        self.base_url = 'https://api.bls.gov/publicAPI/v2/timeseries/data/'
+        self.headers = {'Content-type': 'application/json'}
+        self.start_year = datetime.now().year - self.lookback_years
+        self.end_year = datetime.now().year
 
-    def fetch_bls_data(self, series_ids, start_year, end_year, mom_diff=True):
-        headers = {'Content-type': 'application/json'}
+    def fetch_bls_data(self, series_ids, mom_diff=True):
         data = json.dumps({
             "seriesid": series_ids,
-            "startyear": str(start_year),
-            "endyear": str(end_year),
+            "startyear": str(self.start_year),
+            "endyear": str(self.end_year),
             "registrationkey": self.api_key
         })
-        response = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
+        response = requests.post(self.base_url, data=data, headers=self.headers)
         json_data = json.loads(response.text)
         all_data = []
-        # print (json_data)
         for series in json_data['Results']['series']:
             series_id = series['seriesID']
             for item in series['data']:
@@ -42,61 +46,156 @@ class BLS:
         df = pd.DataFrame(all_data)
         df['date'] = pd.to_datetime(df['year'] + df['period'].str.replace('M', '-'), format='%Y-%m')
         df.sort_values(by=['series_id', 'date'], inplace=True)
-        if mom_diff==True:
+        if mom_diff:
             df['mom_diff'] = df['value'].diff()  # Calculate the month-over-month difference
-            return df
-        else:
-            return df
+        return df
+
+    def fetch_and_save_bls_data(self, series_info, file_format="csv"):
+        dataframes = {}
+        for key, value in series_info.items():
+            dataframes[key] = self.fetch_bls_data(series_ids=value['series_ids'], mom_diff=value.get('mom_diff', True))
+        
+        self.save_data(dataframes, file_format)
 
     def nonfarm_payroll(self):
-        return self.fetch_bls_data(series_ids=["CES0000000001"], 
-                                   start_year=datetime.now().year - self.lookback_years, 
-                                   end_year=datetime.now().year)
-    
+        return self.fetch_bls_data(series_ids=["CES0000000001"])
+
     def unemployment_rate(self):
-        return self.fetch_bls_data(series_ids=["LNS14000000"], 
-                                   start_year=datetime.now().year - self.lookback_years, 
-                                   end_year=datetime.now().year, mom_diff=False)
+        return self.fetch_bls_data(series_ids=["LNS14000000"], mom_diff=False)
 
     def us_job_opening(self):
-        return self.fetch_bls_data(series_ids=["JTS000000000000000JOL"], 
-                                   start_year=datetime.now().year - self.lookback_years, 
-                                   end_year=datetime.now().year)
-    
+        return self.fetch_bls_data(series_ids=["JTS000000000000000JOL"])
+
     def cps_n_ces(self):
-        return self.fetch_bls_data(series_ids=["CES0000000001", "LNS12000000"], 
-                                   start_year=datetime.now().year - self.lookback_years, 
-                                   end_year=datetime.now().year)
+        return self.fetch_bls_data(series_ids=["CES0000000001", "LNS12000000"])
 
     def us_avg_weekly_hours(self):
-        return self.fetch_bls_data(series_ids=["CES0500000002"], 
-                                   start_year=datetime.now().year - self.lookback_years, 
-                                   end_year=datetime.now().year)
+        return self.fetch_bls_data(series_ids=["CES0500000002"])
 
     def unemployment_by_demographic(self):
-        return self.fetch_bls_data(series_ids=["LNS14000009", "LNS14000006", "LNS14000003", "LNS14000000"], 
-                                   start_year=datetime.now().year - self.lookback_years, 
-                                   end_year=datetime.now().year)
-    
+        return self.fetch_bls_data(series_ids=["LNS14000009", "LNS14000006", "LNS14000003", "LNS14000000"])
+
+    def save_data(self, dataframes, file_format="csv"):
+        for name, df in dataframes.items():
+            if file_format == "parquet":
+                df.to_parquet(f"{name}.parquet")
+            elif file_format == "csv":
+                df.to_csv(f"{name}.csv")
+
 if __name__ == "__main__":
     bls = BLS()
-
-    df_unemployment_rate = bls.unemployment_rate()
-    df_nonfarm_payroll = bls.nonfarm_payroll()
-    df_us_job_opening = bls.us_job_opening()
-    df_cps_n_ces = bls.cps_n_ces()
-    df_us_avg_weekly_hours = bls.us_avg_weekly_hours()
-    df_unemployment_by_demographic = bls.unemployment_by_demographic()
     
-    print (df_us_job_opening, df_nonfarm_payroll)
-
-    dataframes = {
-        'us_job_opening': df_us_job_opening,
-        'nonfarm_payroll': df_nonfarm_payroll,
-        'cps_n_ces': df_cps_n_ces,
-        'us_avg_weekly_hours': df_us_avg_weekly_hours,
-        'unemployment_by_demographic': df_unemployment_by_demographic
+    series_info = {
+        'nonfarm_payroll': {'series_ids': ["CES0000000001"]},
+        'unemployment_rate': {'series_ids': ["LNS14000000"], 'mom_diff': False},
+        'us_job_opening': {'series_ids': ["JTS000000000000000JOL"]},
+        'cps_n_ces': {'series_ids': ["CES0000000001", "LNS12000000"]},
+        'us_avg_weekly_hours': {'series_ids': ["CES0500000002"]},
+        'unemployment_by_demographic': {'series_ids': ["LNS14000009", "LNS14000006", "LNS14000003", "LNS14000000"]}
     }
 
-    bls.save_data(dataframes, file_format="parquet")
-    bls.save_data(dataframes, file_format="csv")
+    bls.fetch_and_save_bls_data(series_info, file_format="parquet")
+    bls.fetch_and_save_bls_data(series_info, file_format="csv")
+
+
+# from TAI.utils import ConfigLoader
+# import requests, json
+# import pandas as pd
+# from datetime import datetime
+
+# class BLSAuth:
+#     def __init__(self):
+#         config_loader = ConfigLoader()
+#         self.api_key = config_loader.get_config('BLS', 'api_key')
+
+#     def get_api_key(self):
+#         return self.api_key
+
+# class BLS:
+#     def __init__(self):
+#         auth = BLSAuth()
+#         self.api_key = auth.get_api_key()
+#         self.lookback_years = 5
+
+#     def fetch_bls_data(self, series_ids, start_year, end_year, mom_diff=True):
+#         headers = {'Content-type': 'application/json'}
+#         data = json.dumps({
+#             "seriesid": series_ids,
+#             "startyear": str(start_year),
+#             "endyear": str(end_year),
+#             "registrationkey": self.api_key
+#         })
+#         response = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
+#         json_data = json.loads(response.text)
+#         all_data = []
+#         # print (json_data)
+#         for series in json_data['Results']['series']:
+#             series_id = series['seriesID']
+#             for item in series['data']:
+#                 all_data.append({
+#                     "series_id": series_id,
+#                     "year": item['year'],
+#                     "period": item['period'],
+#                     "value": float(item['value']),
+#                 })
+#         df = pd.DataFrame(all_data)
+#         df['date'] = pd.to_datetime(df['year'] + df['period'].str.replace('M', '-'), format='%Y-%m')
+#         df.sort_values(by=['series_id', 'date'], inplace=True)
+#         if mom_diff==True:
+#             df['mom_diff'] = df['value'].diff()  # Calculate the month-over-month difference
+#             return df
+#         else:
+#             return df
+
+#     def nonfarm_payroll(self):
+#         return self.fetch_bls_data(series_ids=["CES0000000001"], 
+#                                    start_year=datetime.now().year - self.lookback_years, 
+#                                    end_year=datetime.now().year)
+    
+#     def unemployment_rate(self):
+#         return self.fetch_bls_data(series_ids=["LNS14000000"], 
+#                                    start_year=datetime.now().year - self.lookback_years, 
+#                                    end_year=datetime.now().year, mom_diff=False)
+
+#     def us_job_opening(self):
+#         return self.fetch_bls_data(series_ids=["JTS000000000000000JOL"], 
+#                                    start_year=datetime.now().year - self.lookback_years, 
+#                                    end_year=datetime.now().year)
+    
+#     def cps_n_ces(self):
+#         return self.fetch_bls_data(series_ids=["CES0000000001", "LNS12000000"], 
+#                                    start_year=datetime.now().year - self.lookback_years, 
+#                                    end_year=datetime.now().year)
+
+#     def us_avg_weekly_hours(self):
+#         return self.fetch_bls_data(series_ids=["CES0500000002"], 
+#                                    start_year=datetime.now().year - self.lookback_years, 
+#                                    end_year=datetime.now().year)
+
+#     def unemployment_by_demographic(self):
+#         return self.fetch_bls_data(series_ids=["LNS14000009", "LNS14000006", "LNS14000003", "LNS14000000"], 
+#                                    start_year=datetime.now().year - self.lookback_years, 
+#                                    end_year=datetime.now().year)
+    
+# if __name__ == "__main__":
+#     bls = BLS()
+
+#     df_unemployment_rate = bls.unemployment_rate()
+#     df_nonfarm_payroll = bls.nonfarm_payroll()
+#     df_us_job_opening = bls.us_job_opening()
+#     df_cps_n_ces = bls.cps_n_ces()
+#     df_us_avg_weekly_hours = bls.us_avg_weekly_hours()
+#     df_unemployment_by_demographic = bls.unemployment_by_demographic()
+    
+#     print (df_us_job_opening, df_nonfarm_payroll)
+
+#     dataframes = {
+#         'us_job_opening': df_us_job_opening,
+#         'nonfarm_payroll': df_nonfarm_payroll,
+#         'cps_n_ces': df_cps_n_ces,
+#         'us_avg_weekly_hours': df_us_avg_weekly_hours,
+#         'unemployment_by_demographic': df_unemployment_by_demographic
+#     }
+
+#     bls.save_data(dataframes, file_format="parquet")
+#     bls.save_data(dataframes, file_format="csv")
